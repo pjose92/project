5724911028b26d7c82b30be024d4423b3204b171
Jose Perez <44319886+pjose92@users.noreply.github.com>
2023-02-08 21:54:17 -0600

    👌 IMPROVE: prettier format. cr year updated (#1)

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,42 @@
 "use strict";
 
-jQuery(document).ready(function($){
-	
-	/************** Single Page Nav Plugin *********************/
-	$('.menu').singlePageNav(
-		{filter: ':not(.external)'}
-	);
+jQuery(document).ready(function ($) {
+  /************** Single Page Nav Plugin *********************/
+  $(".menu").singlePageNav({ filter: ":not(.external)" });
 
-	/************** FlexSlider Plugin *********************/
-	$('.flexslider').flexslider({
-		animation : 'fade',
-		controlNav : false,
-		nextText : '',
-		prevText : '',
-	});
+  /************** FlexSlider Plugin *********************/
+  $(".flexslider").flexslider({
+    animation: "fade",
+    controlNav: false,
+    nextText: "",
+    prevText: "",
+  });
 
-	$('.flex-caption').addClass('animated bounceInDown');
+  $(".flex-caption").addClass("animated bounceInDown");
 
-	$('.flex-direction-nav a').on('click', function() {
-        $('.flex-caption').removeClass('animated bounceInDown');
-        $('.flex-caption').fadeIn(0).addClass('animated bounceInDown');
-    });
+  $(".flex-direction-nav a").on("click", function () {
+    $(".flex-caption").removeClass("animated bounceInDown");
+    $(".flex-caption").fadeIn(0).addClass("animated bounceInDown");
+  });
 
-	/************** LightBox *********************/
-	$(function(){
-		$('[data-rel="lightbox"]').lightbox();
-	});
+  /************** LightBox *********************/
+  $(function () {
+    $('[data-rel="lightbox"]').lightbox();
+  });
 
-	/************** Go Top *********************/
-	$('#go-top').click(function(event) {
-        event.preventDefault();
-        jQuery('html, body').animate({scrollTop: 0}, 800);
-        return false;
-    });
+  /************** Go Top *********************/
+  $("#go-top").click(function (event) {
+    event.preventDefault();
+    jQuery("html, body").animate({ scrollTop: 0 }, 800);
+    return false;
+  });
 
-    /************** Responsive Navigation *********************/
-	$('.toggle-menu').click(function(){
-        $('.menu').stop(true,true).toggle();
-        return false;
-    });
-    $(".responsive-menu .menu a").click(function(){
-        $('.responsive-menu .menu').hide();
-    });
+  /************** Responsive Navigation *********************/
+  $(".toggle-menu").click(function () {
+    $(".menu").stop(true, true).toggle();
+    return false;
+  });
+  $(".responsive-menu .menu a").click(function () {
+    $(".responsive-menu .menu").hide();
+  });
 });
